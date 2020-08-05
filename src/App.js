3f42f562f612ee01efe2d90a80a67d3caa8720fa
user <user@WKS27042.hm.com>
2020-08-05 23:53:08 +0530

    inline style & component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
 
 class App extends Component {
   render() {
     const flag = false;
     return (
       <div>
-        {2 === '2' ? <h1>hello react</h1> : <h1>calculation wrong</h1>}
-
         {flag == false ? <h1>false calculation</h1> : <h1>true statement</h1>}
+        <Navbar />
       </div>
     );
   }
