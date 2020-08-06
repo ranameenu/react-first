2d4d7dd630d06a75cfef59a1d899a3604992cc27
user <user@WKS27042.hm.com>
2020-08-06 07:32:50 +0530

    routers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './layout/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './component/layout/Navbar';
+import Home from './component/pages/Home';
+import About from './component/pages/About';
+import Contact from './component/pages/Contact';
+import Notfound from './component/pages/Notfound';
+import Users from './component/users/Users';
 
 class App extends Component {
   render() {
-    const flag = false;
     return (
-      <div>
-        {flag == false ? <h1>false calculation</h1> : <h1>true statement</h1>}
+      <BrowserRouter>
         <Navbar />
-      </div>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Notfound} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
